d1c2c57c5041913a1ece1a73a6661c9c866814fe
Mikhail <me@mikhailkarpenko.com>
2021-09-22 21:40:58 +0300

    hw-06

---
+++ hw-06/src/components/Page404/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Typography } from '@material-ui/core';
+
+export const Page404 = () => {
+
+    return (
+      <>
+        <Typography
+          align='center'
+          variant='h2'
+          color='secondary'
+        >404 page</Typography>
+      </>
+    )
+}
